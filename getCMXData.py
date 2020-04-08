b276be401baf66a8aa657c1d7dfc1a9ed51937d5
SenorSmartyPants <senorsmartypants@gmail.com>
2020-04-08 17:11:24 -0500

    Parse and return a lot more data

--- getCMXData.py
+++ getCMXData.py
@@ -2,7 +2,9 @@ import sys
 import argparse
 import re
 import requests 
+from pprint import pprint 
 from bs4 import BeautifulSoup 
+from datetime import datetime
 
 import google
 
@@ -10,18 +12,11 @@ debug = False
 
 CMXBASEURL = "https://www.comixology.com/a/digital-comic/"
 
-
-
-
-
-
-
-
-
-
-
-
-
+def parseMultiple(soup):
+  items = []
+  for item in soup:
+    items.append(item.get_text(strip=True))
+  return items
 
 def findCMXURL(args):
     CMXID = google.findCMXID(args)
@@ -34,22 +29,53 @@ def getResponseByID(CMXID):
   return r
 
 def parseCMX(r, CMXID):
+  soup = BeautifulSoup(r.content, 'html.parser')
   
-  soup = BeautifulSoup(r.content, 'html.parser') 
+  metadata = {}
 
-  title = soup.find('h1', class_='title')
+  #content from head
+  titleVolumeAndIssue = soup.find("meta", attrs={'name':'twitter:title'})['content']
+  match = re.search('(.*?)( \((\d{4})-\d{0,4}\))? #(.*?)( \(of \d\))?$', titleVolumeAndIssue)
+  metadata['series'] = match.group(1)
+  if match.group(2) is not None:
+    metadata['volume'] = match.group(3)
+  metadata['issue'] = match.group(4)
 
-  print(title.get_text(strip=True))
+  metadata['description'] = soup.find("meta", attrs={'name':'description'})['content']
+  metadata['webLink'] = soup.find("meta", attrs={'name':'url'})['content']
+  metadata['coverURL'] = soup.find("meta", attrs={'name':'image'})['content']
 
-  aGenres = soup.find_all('a', href=re.compile('comics-genre'))
 
-  genres = []
+  #content from body
+  metadata['publisher'] = soup.find("h3", attrs={'title':'Publisher'}).get_text(strip=True)
+  #credits
+  metadata['writer'] = parseMultiple(soup.find_all("h2", attrs={'title':'Written by'}))
+  metadata['penciller'] = parseMultiple(soup.find_all("h2", attrs={'title':'Pencils'}))
+  metadata['inker'] = parseMultiple(soup.find_all("h2", attrs={'title':'Inks'}))
+  metadata['colorist'] = parseMultiple(soup.find_all("h2", attrs={'title':'Colored by'}))
+  metadata['coverArtist'] = parseMultiple(soup.find_all("h2", attrs={'title':'Cover by'}))
+  #is art by the same as penciller?
+  metadata['artist'] = parseMultiple(soup.find_all("h2", attrs={'title':'Art by'}))
+  metadata['genres'] = parseMultiple(soup.find_all('a', href=re.compile('comics-genre')))
 
-  for genre in aGenres:
-    genres.append(genre.get_text(strip=True))
+  pageCount = soup.find("h4", text="Page Count").find_next_sibling().get_text(strip=True)
+  pages = re.search('(.*) Pages', pageCount).group(1)
+  metadata['pageCount'] = pages
 
-  genreList = ", ".join(genres)
-  print(genreList)
+  printDateStr = soup.find("h4", text="Print Release Date").find_next_sibling().get_text(strip=True)
+  printDate = datetime.strptime(printDateStr, '%B %d %Y')
+  metadata['Year'] = printDate.year
+  metadata['Month'] = printDate.month
+  metadata['Day'] = printDate.day
+
+  metadata['Notes'] = "Tagged with Comixology-Scraper on {0}. [CMXDB{1}]".format(
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            CMXID)
+
+
+  pprint(metadata)
+
+  return metadata
 
 def byCMXID(args):
   r = getResponseByID(args.CMXID)
