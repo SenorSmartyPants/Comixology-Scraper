37ab08d8e8273fd91a24a7b592980a52b15df58a
SenorSmartyPants <senorsmartypants@gmail.com>
2020-04-24 17:11:18 -0500

    Google results are slightly different in htmlAgilityPack

--- parseGoogle.py
+++ parseGoogle.py
@@ -19,7 +19,7 @@ def parseGoogleResult(URL, debug = False):
         return -1 # don't keep searching
 
     #should be first result - if found
-    aResult = soup.find('a', href=re.compile('https:\/\/(www|m).comixology.com'))
+    aResult = findElement(soup, 'a', 'href', '.comixology.com', substring=True)
 
     if aResult:
         CMXURL = findAttributeValue(aResult, 'href')
