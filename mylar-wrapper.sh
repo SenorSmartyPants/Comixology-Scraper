8d12b90b688efdda45cc71dc3d2a46ce0c4262fd
SenorSmartyPants <senorsmartypants@gmail.com>
2021-05-12 17:45:26 -0500

    Fix path

--- mylar-wrapper.sh
+++ mylar-wrapper.sh
@@ -5,4 +5,4 @@
 # $4 newpath = path to the file after Mylar has processed it
 # $5 metadata = seriesmetadata { name, comicyear, comicid , issueid, issueyear, issue, publisher } (didn't use it but appears to be the same as you state from a casual glance).
 
-/usr/bin/python3 /app/comictagger.py -S /storage/Comics/Comixology-Scraper/mylar.py "$4"
+/usr/bin/python3 /app/comictagger.py -S /scripts/Comixology-Scraper/mylar.py "$4"
